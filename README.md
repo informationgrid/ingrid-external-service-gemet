f939f70df54fb13d893f4d30893ea2008bdf0ea3
Martin Maidhof <martin@wemove.com>
2017-05-10 15:16:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# InGrid External Service GEMET
+=========
+
+This library is part of the InGrid software package. It provides the GEMET thesaurus functionality using the EIONET Webservice. It is used in the SNS iPlug and the InGrid Editor.
+
+
+Contribute
+----------
+
+- Issue Tracker: https://github.com/informationgrid/ingrid-external-service-gemet/issues
+- Source Code: https://github.com/informationgrid/ingrid-external-service-gemet
+ 
+### Set up eclipse project
+
+```
+mvn eclipse:eclipse
+```
+
+and import project into eclipse.
+
+Support
+-------
+
+If you are having issues, please let us know: info@informationgrid.eu
+
+License
+-------
+
+The project is licensed under the EUPL license.
---
+++ src/main/java/de/ingrid/external/gemet/GEMETService.java
@@ -0,0 +1,72 @@
+package de.ingrid.external.gemet;
+
+import java.util.Locale;
+
+import de.ingrid.external.ThesaurusService;
+import de.ingrid.external.om.RelatedTerm;
+import de.ingrid.external.om.Term;
+import de.ingrid.external.om.TreeTerm;
+
+public class GEMETService implements ThesaurusService {
+
+    @Override
+    public Term[] findTermsFromQueryTerm(String arg0, MatchingType arg1, boolean arg2, Locale arg3) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Term[] findTermsFromQueryTerm(String arg0, String arg1, MatchingType arg2, boolean arg3, Locale arg4) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public TreeTerm[] getHierarchyNextLevel(String arg0, Locale arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public TreeTerm[] getHierarchyNextLevel(String arg0, String arg1, Locale arg2) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public TreeTerm getHierarchyPathToTop(String arg0, Locale arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public TreeTerm getHierarchyPathToTop(String arg0, String arg1, Locale arg2) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public RelatedTerm[] getRelatedTermsFromTerm(String arg0, Locale arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Term[] getSimilarTermsFromNames(String[] arg0, boolean arg1, Locale arg2) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Term getTerm(String arg0, Locale arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Term[] getTermsFromText(String arg0, int arg1, boolean arg2, Locale arg3) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
